aae95344b426834aabc69135de861b0a9505c5dd
RITU SINGH <107137060+rs6104@users.noreply.github.com>
2023-09-20 12:09:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ if(close){
         nav.classList.remove('active');
     })
 } 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const emailInput = document.getElementById("emailInput");
+    const signUpButton = document.querySelector("#newsletter button");
+    const emailDisplay = document.querySelector("#newsletter .email-display");
+
+    signUpButton.addEventListener("click", function () {
+        const email = emailInput.value;
+
+        if (isValidEmail(email)) {
+            displayEmail(email);
+        } else {
+            alert("Please enter a valid email address.");
+        }
+    });
+    function isValidEmail(email) {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email);
+    }
+    function displayEmail(email) {
+        emailDisplay.textContent = `You are subscribed with the email: ${email}`;
+        emailInput.value = '';
+    }
+});
